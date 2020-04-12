d9794aa828aaa718af0b460bc934dfe36cfccc30
gothcorgi <61165454+gothcorgi@users.noreply.github.com>
2020-04-11 21:21:20 -0400

    commit

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] popUps;
+    private int popupIndex;
+    public GameObject spawner;
+    public float waitTime = 2f;
+    public Player player;
+
+    private void Start()
     {
-        
+        player.jumpForce = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if (i == popupIndex)
+            {
+                popUps[i].SetActive(true); 
+            }else
+            {
+                popUps[i].SetActive(false); 
+            }
+        }
+        if (popupIndex == 0)
+            if(Input.GetKeyDown(KeyCode.LeftArrow)|| Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                popupIndex++; 
+            }else if(popupIndex == 1)
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    player.jumpForce = 8;
+                    popupIndex++; 
+                }else if (popupIndex == 2)
+                {
+                    if (waitTime <= 0) ;
+                    {
+                        spawner.SetActive(true);
+                    }else
+                    {
+                        waitTime -= Time.deltaTime;
+                    }
+                    
+                }
+            }
+        {
+
+        }
     }
 }
